cc841f0dcd40df9d0b577145476fafac2841f65f
kok liu <kokdemo@gmail.com>
2014-08-14 00:20:13 +0800

    add time mode!
    增加计时模式，修复了我所知的所有bug

--- js/main.js
+++ js/main.js
@@ -5,6 +5,21 @@ $(document).ready(function() {
 	$("#ce").click(function() {ce()});
 });
 
+window.addEventListener('shake', ce(), false);
+
+function clock(){
+    var s=60;
+    var int = setInterval(function(){
+        s=s-1;
+        $("#title").text(s);
+        if (s<=0){
+        clearInterval(int);
+        lose();
+        }
+    }, 1000);
+
+}
+
 function inputall(){
 	inputnumber("#1");
 	inputnumber("#2");
@@ -26,13 +41,13 @@ function inputall(){
 
 function ce() {
 	$("#equation").val("");
-	$(".numbtn").fadeIn();
-};
+	$("#number div,#number button").show();
+    document.title = "Cool24";
+}
 
 function add(x) {
-	var inputnew = $('#equation').val()+ $(x).val();
-	$('#equation').val(inputnew);
-};
+	$('#equation').val($('#equation').val()+ $(x).val());
+}
 
 function inputnumber(x) {
 	$(x).click(function() {
@@ -41,7 +56,7 @@ function inputnumber(x) {
 		$(".numbtn").fadeOut();
 		$(".signbtn").fadeIn();
 	});
-};
+}
 
 function inputsign(x) {
 	$(x).click(function() {
@@ -49,17 +64,36 @@ function inputsign(x) {
 		$(".signbtn").fadeOut();
 		$(".numbtn").fadeIn();
 	});
-};
+}
+
+function lose(){
+    alert("时间结束！你失败了!");
+    if(document.body.clientWidth<700){
+        $("#gamearea").html("<h1 class='center'>快到右上角分享给你的微信好友吧！</h1>");
+        document.title = "我居然败给这个24点游戏，快来帮我挑战一下!";
+    }
+}
+
 
 function count() {
-	var answer = 0;
+    var words="";
 	var count = $("#equation").val();
-	answer = eval(count);
-	if (answer==24) {
+    var evals = eval(count);
+	var answer = Math.abs(evals-24);
+
+	if (answer<0.5) {
 		words = "你胜利了!\n你的最终结果为"+count+"=24";
+        if(document.body.clientWidth<700){
+            $("#gamearea").html("<h1 class='center'>快到右上角分享给你的微信好友吧！</h1>");
+            document.title = "我轻松的做出了这个24点，你也来挑战一下!";
+        }
 	} else{
-		words = "你失败了!\n你的最终结果为"+count+"="+answer;
-	};
+        words = "你失败了!\n"+"你的最终结果为"+count+"="+evals;
+        if(document.body.clientWidth<700){
+            $("#gamearea").html("<h1 class='center'>快到右上角分享给你的微信好友吧！</h1>");
+            document.title = "我居然败给这个24点游戏，快来帮我挑战一下!";
+        }
+	}
 	alert(words);
 	return words;
-};
+}
--- js/rand.js
+++ js/rand.js
@@ -1,34 +1,81 @@
-function getRandom(n) {
-	return Math.floor(Math.random() * n + 1)
-};
-var num1;
-var num2;
-var num3;
-var num4;
-var randomtime = 0;
-
-function allrandom() {
-	num1 = getRandom(10);
-	num2 = getRandom(10);
-	num3 = getRandom(10);
-	num4 = getRandom(10);
-	randomtime = randomtime + 1;
+var numberList= new Array(4);
+function getRandom() {	
+	for (var i = 3; i >= 0; i--) {
+		numberList[i] = Math.floor(Math.random() * 10 + 1);
+	};
+	return numberList;
 };
 
-function input2() {
-	$("#1").text(num1).val(num1);
-	$("#2").text(num2).val(num2);
-	$("#3").text(num3).val(num3);
-	$("#4").text(num4).val(num4);
+function inputNumber(numberList) {
+	for (var i = 3; i >= 0; i--) {
+		$("#"+(i+1)).text(numberList[i]).val(numberList[i]);
+	};
 };
+
+var nextPar;
+function changeURL(){
+	var local_url = document.location.href;
+	var nextPar = local_url.indexOf("?");
+	if (nextPar == -1) {
+		var	get_par = local_url.slice(0,local_url.length);
+	}else{
+		var	get_par = local_url.slice(0,nextPar);
+	};
+    get_par = get_par+"?"+numberList[0]+","+numberList[1]+","+numberList[2]+","+numberList[3];
+    var state = ( {
+        url: get_par, title: "Cool24"
+    } );
+    window.history.pushState(state, "Cool24", get_par);
+    //document.location.href = get_par;
+	return get_par;
+}
+
+function getURLPar() {
+	var get_par = "";
+	var local_url = document.location.href;
+	var nextPar = local_url.indexOf("?");
+	if (nextPar != -1) {
+		get_par = local_url.slice(nextPar+1, local_url.length);
+	};
+	var substr = ",";
+	var arrTmp = new Array();
+	var i = 0,
+		j = 0,
+		k = get_par.length;
+	while (i < k) {
+		j = get_par.indexOf(substr, i);
+		if (j != -1) {
+			if (get_par.substring(i, j) != "") {
+				arrTmp.push(get_par.substring(i, j));
+			}
+			i = j + 1;
+		} else {
+			if (get_par.substring(i, k) != "") {
+				arrTmp.push(get_par.substring(i, k));
+			}
+			i = k;
+		}
+	}
+	return arrTmp;
+};
+
+function addwords(array){
+    $("#gamearea").fadeIn();
+    inputNumber(array);
+    $("#number button").addClass("btn btn-large btn-block btn-info");
+	$("#sign button").addClass("btn btn-large btn-block btn-success");
+    $("#gamestart").text("再来一盘");
+    clock($("#title"));
+}
+
 $(document).ready(function() {
+	var arrayList = getURLPar();
+	if (arrayList.length != 0) {
+        addwords(arrayList)
+	};
 	$("#gamestart").click(function() {
-		allrandom();
-		input2();
-		$("#gamearea").fadeIn();
-		ce();
-		$("#number botton").addClass("btn btn-large btn-block btn-info");
-		$("#sign botton").addClass("btn btn-large btn-block btn-success");
-		$("#gamestart").text("再来一盘");
+		addwords(getRandom());
+        ce();
+        changeURL();
 	});
 });
